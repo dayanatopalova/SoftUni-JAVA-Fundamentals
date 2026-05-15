978f5854fe4227493fc63e52e03f934a780c3550
dayat@Bounty <stu2501261018@uni-plovdiv.bg>
2026-05-15 09:52:40 +0300

    decrypting messages added

---
+++ Data-Types/Exercise1-Additional-Problems/src/DecryptingMessages.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DecryptingMessages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int key = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+
+        StringBuilder message = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            char ch = scanner.nextLine().charAt(0);
+
+            ch += (char) key;
+            message.append(ch);
+        }
+
+        System.out.println(message);
+    }
+}
